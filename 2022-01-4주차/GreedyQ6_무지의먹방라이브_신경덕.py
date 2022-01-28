d75eddb5a451a0f5ea0b83fc0b6371f29e045c2a
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-28 19:47:29 +0900

    [ADD] (greedy) 무지의 먹방 라이브 설명 추가

--- 2022-01-4주차/GreedyQ6_무지의먹방라이브_신경덕.py
+++ 2022-01-4주차/GreedyQ6_무지의먹방라이브_신경덕.py
@@ -1,3 +1,38 @@
+"""
+3,1,2 5 ==> 1
+
+- 값에 index값을 삽입
+prev = 0
+food_len = 3
+k = 5
+pq = [3,0] [1,1] [2,2]
+
+- 0번째 값으로 정렬 
+prev = 0
+food_len = 3
+k = 5
+pq = [1,1] [2,2] [3,0]
+
+- pq에서 pop, 값처리
+prev = 0
+food_len = 3
+k = 5 - (1 - 0) * 3 ==> 5 - 3 ==> 2 
+pq = [2,2] [3,0]
+
+- pq에서 pop, 값처리 
+prev = 1
+food_len = 2
+k = 2 - (2 - 1) * 2 ==> 2 - 2 ==> 0
+pq = [3,0]
+
+
+- pq의 남은값으로 원래 index로 다시 정렬 해서 값 찾기
+prev = 2
+food_len = 1
+pq = [3,0]
+
+"""
+
 import heapq
 
 def solution(food_times, k):
@@ -21,3 +56,4 @@ def solution(food_times, k):
             break
 
     return answer 
+
