60a21599812b9b043a599e651af65d0858e5e279
samseburn <90seok_0@naver.com>
2022-01-28 21:51:37 +0900

    곱하기혹은더하기 문제풀이

---
+++ 2022-01-4주차/GreedyQ2_곱하기혹은더하기_고석영.py
@@ -0,0 +1,13 @@
+s = input()
+result = 0
+
+for x in s:
+    if int(x) <= 1:
+        result += int(x)
+    else:
+        if result <= 1:
+            result += int(x)
+        else:
+            result *= int(x)
+
+
