8cccdfe9b8a894ad6c938c166d1bdd7468bf9f89
samseburn <90seok_0@naver.com>
2022-03-24 20:42:01 +0900

    토마토

---
+++ 2022-03-3주차/DFS&BFS_토마토_고석영.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = dx[i] + x, dy[i] + y
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append([nx, ny])
+ 
+m, n = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map, int, input().split()))
+
+queue = deque([])
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+result = 0
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            queue.append([i, j])
+                           
+bfs()
+for i in graph:
+    for j in i:
+        if j == 0:
+            print(-1)       
+    result = max(result, max(i))
+print(result - 1)
