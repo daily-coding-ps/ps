4cdbc67a162182cd587c3b7b4520137561aac91d
Eun <43868540+JegalEun@users.noreply.github.com>
2022-04-11 19:17:29 +0900

    Create dp_멀리뛰기_제갈은.java

---
+++ 2022-04-2주차/dp_멀리뛰기_제갈은.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(int n) {
+        
+         int dp[]=new int[2001];
+        
+        dp[1]=1;
+        dp[2]=2;
+       
+        for(int i=3;i<dp.length;i++) {
+        	dp[i]=(dp[i-1]+dp[i-2])%1234567;
+        }
+        
+        return dp[n];
+    }
+}
