f0f78f6bfc5c51421920a4c1d2a3eec0e30a68da
samseburn <90seok_0@naver.com>
2022-04-21 20:14:11 +0900

    거리두기 확인하기

--- 2022-04-3주차/random_거리두기 확인하기_고석영.py
+++ 2022-04-3주차/random_거리두기 확인하기_고석영.py
@@ -1,12 +1,9 @@
-'''
-거리두기를 지키고 있으면 1, 아니라면 0을 반환
-'''
 from collections import deque
 
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, -1]
 
 def bfs(place):
     start = []
+    
     for i in range(5):
         for j in range(5): 
             if place[i][j] == 'P':
@@ -19,20 +16,21 @@ def bfs(place):
         visited[s[0]][s[1]] = 1 # 시작점은 확인했으니 visited에서 1로 체크
         
         while queue: 
-            x, y = queue.popleft()
+            y, x = queue.popleft() # 행, 열로 계산하기 쉽게 y, x로 시작점 계산 
             dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1] # 상하좌우
             
             for i in range(4): 
                 nx = x + dx[i]
                 ny = y + dy[i]
+                
                 if 0 <= nx < 5  and 0 <= ny < 5 and visited[ny][nx] == 0: # 범위를 벗어나지 않는 조건에서
-                    if place[nx][ny]  == '0':
-                        queue.append([nx, ny])
-                        visited[nx][ny] = 1
-                        distance[nx][ny] = distance[x][y] + 1
+                    if place[ny][nx]  == 'O': # 상하좌우 중 빈자리 발견시
+                        queue.append([ny, nx])
+                        visited[ny][nx] = 1 
+                        distance[ny][nx] = distance[y][x] + 1
                     
                     # 거리두기가 지켜지지 않았을 경우
-                    if place[nx][ny]  == 'P' and distance[x][y] <= 1: 
+                    if place[ny][nx]  == 'P' and distance[y][x] <= 1: 
                         return 0
     return 1
             
