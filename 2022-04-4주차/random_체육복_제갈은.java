5cb602c403e61ca74e0d72a9db0c5f6d36378039
Eun <43868540+JegalEun@users.noreply.github.com>
2022-04-25 22:49:37 +0900

    Create random_체육복_제갈은.java

---
+++ 2022-04-4주차/random_체육복_제갈은.java
@@ -0,0 +1,32 @@
+public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        
+        answer+=n-lost.length;
+        
+       for(int i=0;i<lost.length;i++) {
+    	   for(int j=0;j<reserve.length;j++) {
+    		   if(lost[i]==reserve[j]) {
+    			  answer++;
+    			  lost[i]=-1;
+    			  reserve[j]=-1;
+    			  break;
+    		   }
+    	   }
+       }
+       // 도난 + 여분 있는 학생 제거
+        
+       for(int i=0;i<lost.length;i++) {
+    	   for(int j=0;j<reserve.length;j++) {
+    		   if(lost[i]-1==reserve[j]) {
+    			   answer++;
+    			   reserve[j]=-1;
+    			   break;
+    		   }else if(lost[i]+1==reserve[j]) {
+    			   answer++;
+    			   reserve[j]=-1;
+    			   break;
+    		   }
+    	   }
+       }
+        return answer;
+    }
