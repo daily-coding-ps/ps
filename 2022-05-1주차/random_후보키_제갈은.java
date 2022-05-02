42eca492966a80d842aa2e915d4990fa7ebd2294
Eun <43868540+JegalEun@users.noreply.github.com>
2022-05-02 17:47:36 +0900

    random_후보키_제갈은(코드수정)

--- 2022-05-1주차/random_후보키_제갈은.java
+++ 2022-05-1주차/random_후보키_제갈은.java
@@ -8,68 +8,71 @@ class Solution {
         int answer = 0;
         int size = relation[0].length;
         
-        for(int i=0;i<size;i++){
-            visited = new boolean[size];
-            dfs(0,i+1, relation);
-        }
-        answer=candiKey.size();
+        visited = new boolean[size];
         
+        for(int i=0;i<size;i++) {
+        	dfs(i+1,0,relation);
+        }
+        
+        answer = candiKey.size();
         return answer;
     }
     
-    public static void dfs(int depth, int len, String[][] relation) {
-    	if(depth == len) {
+    public static void dfs(int len, int depth, String[][] relation) {
+    	
+    	if(len==depth) {
+    		// 조합 만들었을 때
+    		String key = "";
     		ArrayList<Integer> list = new ArrayList<>();
-    		String key="";
     		for(int i=0;i<visited.length;i++) {
-    			// 조합 만들기
     			if(visited[i]) {
     				key+=String.valueOf(i);
     				list.add(i);
-    				// 0, 1, 2, 3, 01, 02, 03, 12, 13, 23 , 012, 013 ...
+    				// 0, 1, 2, 3, 01, 02, 03, ...
     			}
     		}
     		
-    		Map<String, Integer> map = new HashMap<>();
+    		HashMap<String, Integer> map = new HashMap<>();
     		
+    		// 유일성 체크
     		for(int i=0;i<relation.length;i++) {
-    			String s= "";
-    			for(int j=0;j<list.size();j++) {
-    				int index = list.get(j);
-    				s+=relation[i][index];
+    			String s="";
+    			for(Integer j : list) {
+    				s+=relation[i][j];
     			}
-    			
     			if(map.containsKey(s)) {
-    				//값이 유일하지 않을 경우
-    				//유일성 만족 x
     				return;
     			}else {
-    				// 후보키 설정
-    				// key, value
-    				map.put(s,0);	
+    				map.put(s,0);
     			}
     		}
     		
-    		for(String s : candiKey) {
-    			int count=0;
+    		// 최소성 체크
+    		for(String check : candiKey) {
+    			int count = 0;
     			for(int i=0;i<key.length();i++) {
     				String temp = String.valueOf(key.charAt(i));
-    				if(s.contains(temp)) count++;
+    				if(check.contains(temp)) {
+    					count++;
+    				}
     			}
-    			if(count==s.length()) return;
-    			// 후보키가 이미 있으면 return;
+    			if(count==check.length()) return;
     		}
     		candiKey.add(key);
-    		return;
     	}
     	
-    	for(int i=0;i<visited.length;i++) {
-    		if(!visited[i]) {
-    			visited[i]=true;
-    			dfs(depth+1, len, relation);
-    			visited[i]=false;
-    		}
+    	else {
+
+        	// dfs
+        	for(int i=0;i<visited.length;i++) {
+            	if(!visited[i]) {
+            		visited[i]=true;
+            		dfs(len,depth+1,relation);
+            		visited[i]=false;
+            	}
+        	}
     	}
+    	
     }
     
 }
