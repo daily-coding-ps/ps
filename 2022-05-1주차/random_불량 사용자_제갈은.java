91577cb8afe0ebea87fb33769a0d1ebda6dc3f04
Eun <43868540+JegalEun@users.noreply.github.com>
2022-05-05 15:13:58 +0900

    [dfs] 불량 사용자_제갈은_코드수정

--- 2022-05-1주차/random_불량 사용자_제갈은.java
+++ 2022-05-1주차/random_불량 사용자_제갈은.java
@@ -1,53 +1,57 @@
 import java.util.*;
 class Solution {
-    
     static boolean visited[];
+    static boolean flag=false;
     static HashSet<String> hash;
-    static boolean flag;
     
     public int solution(String[] user_id, String[] banned_id) {
         int answer = 0;
         visited = new boolean[user_id.length];
         hash = new HashSet<>();
-        flag = false;
         dfs(user_id,banned_id, 0);
         answer=hash.size();
         return answer;
     }
     
-    public static void dfs(String[] user_id, String[] banned_id, int idx) {
+    public static void dfs(String[] user_id, String[] banned_id, int len){
         
-        if(idx==banned_id.length){
-            StringBuilder sb = new StringBuilder();
+        if(len==banned_id.length){
+            // 2개를 뽑았을 때
+            String s = "";
             for(int i=0;i<user_id.length;i++){
                 if(visited[i]){
-                    sb.append(user_id[i]);
+                    s+=user_id[i];
                 }
             }
-            hash.add(sb.toString());
+            hash.add(s);
             return;
         }else {
             for(int i=0;i<user_id.length;i++){
+                flag=false;
                 if(!visited[i]){
-                    if(user_id[i].length()==banned_id[idx].length()){
-                        // 문자열의 길이가 같다면
-                        flag=false;
+                    if(user_id[i].length()==banned_id[len].length()){
+                        // 길이가 같다면
                         for(int j=0;j<user_id[i].length();j++){
-                            if(banned_id[idx].charAt(j)=='*') continue;
-                            if(user_id[i].charAt(j)!=banned_id[idx].charAt(j)){
+                            if(user_id[i].charAt(j)==banned_id[len].charAt(j)){
+                                continue;
+                            }else if(banned_id[len].charAt(j)=='*'){
+                                continue;
+                            }else {
+                                //문자가 다를 때
                                 flag=true;
                                 break;
                             }
-                         }
+                        }
+                        
                         if(!flag){
-                            // 문자열이 다 같다면
-                            visited[i]=true;
-                            dfs(user_id, banned_id, idx+1);
-                            visited[i]=false;
+                        // 제재 아이디라면
+                        visited[i]=true;
+                        dfs(user_id, banned_id, len+1);
+                        visited[i]=false;
                         }
                     }
                 }
             }
         }
-    } 
+    }
 }
