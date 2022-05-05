0ece92dea76c84f2ea87c66835d46f27bbbdba0d
Eun <43868540+JegalEun@users.noreply.github.com>
2022-05-05 16:17:53 +0900

    Update random_후보키_제갈은.java

--- 2022-05-1주차/random_후보키_제갈은.java
+++ 2022-05-1주차/random_후보키_제갈은.java
@@ -2,77 +2,67 @@ import java.util.*;
 class Solution {
     
     static boolean visited[];
-    static ArrayList<String> candiKey = new ArrayList<>();
+    static ArrayList<String> candi = new ArrayList<>();
     
     public int solution(String[][] relation) {
         int answer = 0;
-        int size = relation[0].length;
+        visited = new boolean[relation[0].length];
         
-        visited = new boolean[size];
-        
-        for(int i=0;i<size;i++) {
-        	dfs(i+1,0,relation);
+        for(int i=0;i<visited.length;i++){
+            dfs(relation, i+1, 0);
         }
-        
-        answer = candiKey.size();
+        answer=candi.size();
         return answer;
     }
     
-    public static void dfs(int len, int depth, String[][] relation) {
-    	
-    	if(len==depth) {
-    		// 조합 만들었을 때
-    		String key = "";
-    		ArrayList<Integer> list = new ArrayList<>();
-    		for(int i=0;i<visited.length;i++) {
-    			if(visited[i]) {
-    				key+=String.valueOf(i);
-    				list.add(i);
-    				// 0, 1, 2, 3, 01, 02, 03, ...
-    			}
-    		}
-    		
-    		HashMap<String, Integer> map = new HashMap<>();
-    		
-    		// 유일성 체크
-    		for(int i=0;i<relation.length;i++) {
-    			String s="";
-    			for(Integer j : list) {
-    				s+=relation[i][j];
-    			}
-    			if(map.containsKey(s)) {
-    				return;
-    			}else {
-    				map.put(s,0);
-    			}
-    		}
-    		
-    		// 최소성 체크
-    		for(String check : candiKey) {
-    			int count = 0;
-    			for(int i=0;i<key.length();i++) {
-    				String temp = String.valueOf(key.charAt(i));
-    				if(check.contains(temp)) {
-    					count++;
-    				}
-    			}
-    			if(count==check.length()) return;
-    		}
-    		candiKey.add(key);
-    	}
-    	
-    	else {
-
-        	// dfs
-        	for(int i=0;i<visited.length;i++) {
-            	if(!visited[i]) {
-            		visited[i]=true;
-            		dfs(len,depth+1,relation);
-            		visited[i]=false;
-            	}
-        	}
-    	}
-    	
+    public static void dfs(String[][] relation, int len, int depth){
+        
+        if(len==depth){
+            String s = "";
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int i=0;i<visited.length;i++){
+                if(visited[i]){
+                    s+=String.valueOf(i);
+                    list.add(i);
+                }
+            }
+            
+            HashSet<String> hash = new HashSet<>();
+            
+            for(int i=0;i<relation.length;i++){
+                String d = "";
+                for(Integer j : list){
+                    d+=relation[i][j];
+                }
+                
+                if(hash.contains(d)){
+                    return;
+                }else {
+                    hash.add(d);
+                }
+            }
+            
+            for(String check : candi){
+                int count=0;
+                for(int i=0;i<s.length();i++){
+                    String temp = String.valueOf(s.charAt(i));
+                    if(check.contains(temp)){
+                        count++;
+                    }
+                    if(count==check.length()) return;
+                }
+            }
+            
+            candi.add(s);
+        }
+        else {
+            for(int i=0;i<relation[0].length;i++){
+                if(!visited[i]){
+                    visited[i]=true;
+                    dfs(relation, len, depth+1);
+                    visited[i]=false;
+                }
+            }
+        }
     }
-    
 }
