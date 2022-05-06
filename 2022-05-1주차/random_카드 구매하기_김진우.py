54c90c68f8719ad36bf318581da5f69011305453
tigre911 <94420939+tigre911@users.noreply.github.com>
2022-05-06 21:33:33 +0900

    수정

--- 2022-05-1주차/random_카드 구매하기_김진우.py
+++ 2022-05-1주차/random_카드 구매하기_김진우.py
@@ -1,35 +1,3 @@
-'''# 첫째 줄에 민규가 구매하려고 하는 카드의 개수 N이 주어진다. (1 ≤ N ≤ 1,000)
-#
-# 둘째 줄에는 Pi가 P1부터 PN까지 순서대로 주어진다. (1 ≤ Pi ≤ 10,000)
-
-n = int(input())
-
-card_list = list(map(int, input().split()))
-# print(f' card[0] : {card_list[0]}')
-# print(card_list)
-ans = []
-for i in range(1, n+1):
-    ans.append((n//i)*card_list[i-1])
-
-# print(ans)
-# print(int(max(ans)))
-
-max_num = int(max(ans))
-max_index = ans.index(max_num)
-# print("index : "  , ans.index(max_num))
-if n - ans.index(max_num)>0:
-    ans.pop(ans.index(max_num))
-    max_num = int(max(ans))
-    # print(max_num)
-    # max_index = ans.index(max_num)
-    # print(n-ans.index(max_num)-2)
-    # print("card : " , card_list[n - ans.index(max_num)-2]) # 4 - 3 - 1
-    max_num += card_list[n - ans.index(max_num)-2]
-# print(f'ans : {ans}')
-# print(f'max_num : {max_num}')
-# print(f'idx : {max_index}')
-
-print(max_num)'''
 
 n = int(input())
 d = [0] * (n + 1)
