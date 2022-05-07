b6ca57cec70eed47eaea96fb569705cc3771c8ee
I C-AN <chicori3@naver.com>
2022-05-08 01:01:10 +0900

    feat: 카드 구매하기

---
+++ 2022-05-1주차/random_카드 구매하기_이시안.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] map = new int[N + 1];
+        int[] dp = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + map[j]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
