6d1c0d2947f301d22b82e7b57adf9b5d061ab746
Yongbin Lee <bin352500@naver.com>
2022-05-12 00:09:36 +0900

    fix: 카드 구매하기

--- 2022-05-1주차/random_카드 구매하기_이용빈.java
+++ 2022-05-1주차/random_카드 구매하기_이용빈.java
@@ -14,9 +14,11 @@ public class 카드 구매하기 {
         for (int i = 0; i < N; i++) {
             cost[i + 1] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= i; j++) {
-                dp[i] = Math.max(dp[i], dp[i - j] + cost[j]);
+        dp[1] = cost[1];
+        for (int i = 2; i <= N; i++) {
+            dp[i] = cost[i];
+            for (int j = 1; j <= i / 2; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + dp[j]);
             }
         }
         System.out.println(dp[N]);
